fcee3d20308f5349c58d8186bb726c60d81c57b7
Charles Sharp <25charlessharp@usd266.com>
2023-02-06 12:13:08 -0600

    done with hellos

--- Loops.java
+++ Loops.java
@@ -1,8 +1,25 @@
 public class Loops {
 
-  // static methods go here
+  public static void nHellos(int x){
+    int n = 1;
+    for(n = 1; n <= x; n ++){
+      if(n % 10 == 1){
+        System.out.println(n + "st Hello");
+      }
+      else if(n % 10 == 2){
+      System.out.println(n + "nd Hello");
+    }
+      else if(n % 10 == 3){
+        System.out.println(n + "rd Hello");
+      }
+      else{
+        System.out.println(n + "th Hello");
+      }
+    }
+  }
 
   public static void main(String[] args) {
+    nHellos(999);
 
     // testing of static methods goes here
 
